6157d1c28b2fc4f9744e010b57746fb951d86619
Radhika108 <rads456@gmail.com>
2022-11-10 06:34:06 +0200

    pipeline

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
-package main
+package hello
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func RegisterHttpHandlers() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "you've requested path: %s\n", r.URL.Path)
-	})
-}
-
-func main() {
-	RegisterHttpHandlers()
-	print("\nlistening on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+func Hello() string {
+	return "Hello, world."
 }
